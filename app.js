ca86ecae52b6e5385231fa8d8d40d161ffa47394
Brian Kim <bkim121@gmail.com>
2017-09-09 10:44:52 -0700

    finished

--- app.js
+++ app.js
@@ -1,1 +1,96 @@
 // TODO
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			done: false
+		};
+	}
+
+	onListItemHover() {
+		this.setState({
+			done: !this.state.done
+		})
+	}
+
+	render() {
+
+		var style = {
+			fontWeight: this.state.done ? '700' : '100'
+		};
+
+		return (
+			<li style={style} onMouseEnter={this.onListItemHover.bind(this)}>{this.props.todo}</li>
+		);
+
+	}
+}
+
+
+
+
+
+// class GroceryListItem extends React.Component {
+// 	constructor(props){
+// 		super(props);
+// 	}
+
+// 	render(){
+// 		return (
+// 			<li>{this.props.todo}</li>
+// 		)
+// 	}
+
+// }
+
+var GroceryList = (props) => (
+	<ul>
+		{props.todos.map(todo =>
+			<GroceryListItem todo={todo} />
+		)}
+	</ul>
+);
+
+
+
+
+
+// var GroceryList= (props) => {
+
+// 	var onListItemClick = (event) => {
+// 		console.log('I got clicked');
+// 	}
+
+// 	return (
+// 		<ul>
+// 			<li onClick={onListItemClick}>{props.todos[0]}</li>
+// 			<li>{props.todos[1]}</li>
+// 			<li>{props.todos[2]}</li>
+// 		</ul>
+// 	)
+// }
+
+
+
+
+
+
+// var GroceryList = (props) => (
+// 	<ul>
+// 		<li>{props.todos[0]}</li>
+// 		<li>{props.todos[1]}</li>
+// 		<li>{props.todos[2]}</li>
+// 	</ul>
+// )
+
+var App = () => (
+	<div>
+		<h2>Grocery List</h2>
+		<GroceryList todos={['Cucumbers', 'Kale', 'Potatoes']} />
+	</div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
